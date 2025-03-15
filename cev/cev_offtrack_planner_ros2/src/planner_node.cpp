7f3f175c335d51af3675e940818d293da466f747
Sidharth Rao <sidharthmrao@gmail.com>
2025-03-15 03:27:40 -0400

    Planner reads costmap from map server.

--- cev/cev_offtrack_planner_ros2/src/planner_node.cpp
+++ cev/cev_offtrack_planner_ros2/src/planner_node.cpp
@@ -12,9 +12,8 @@
 // CEV
 #include <cev_msgs/msg/trajectory.hpp>
 #include "local_planning/mpc.h"
-#include "cost_map/gaussian_conv.h"
-#include "cost_map/global.h"
-#include "cost_map/nearest.h"
+#include "pose.h"
+#include "cost_map/simple.h"
 
 using namespace cev_planner;
 
@@ -22,6 +21,8 @@ class PlannerNode : public rclcpp::Node {
 public:
     PlannerNode(): Node("planner_node"), tf_broadcast_(this) {
         RCLCPP_INFO(this->get_logger(), "Initializing planner node");
+
+        // Init Planner
         Dimensions dimensions = Dimensions{1, 1, 1};
         Constraints full_constraints = Constraints{
             {-1000, 1000},  // x
@@ -34,123 +35,50 @@ public:
 
         local_planner = std::make_shared<local_planner::MPC>(dimensions, full_constraints);
 
-        map_sub_ = this->create_subscription<nav_msgs::msg::OccupancyGrid>("map", 1,
-            std::bind(&PlannerNode::map_callback, this, std::placeholders::_1));
+        // Subscribers
+        map_sub_ = this->create_subscription<nav_msgs::msg::OccupancyGrid>("/offtrack_server/map",
+            1, std::bind(&PlannerNode::map_callback, this, std::placeholders::_1));
 
-        target_rviz_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>("goal_pose",
+        costmap_sub_ =
+            this->create_subscription<nav_msgs::msg::OccupancyGrid>("/offtrack_server/costmap", 1,
+                std::bind(&PlannerNode::costmap_callback, this, std::placeholders::_1));
+
+        target_rviz_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>("/goal_pose",
             1, std::bind(&PlannerNode::target_callback, this, std::placeholders::_1));
 
-        odom_pub_ = this->create_publisher<nav_msgs::msg::Odometry>("/odometry", 1);
-
-        // RVIZ Debug
-        local_path_pub_ = this->create_publisher<nav_msgs::msg::Path>("local_path", 1);
-        cost_map_pub_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>("cost_map", 1);
+        // Publishers
+        odom_pub_ = this->create_publisher<nav_msgs::msg::Odometry>("/odometry/truth", 1);
+        local_path_pub_ = this->create_publisher<nav_msgs::msg::Path>("planner/local_path", 1);
 
         timer_ = this->create_wall_timer(std::chrono::milliseconds(int(dt * 1000)),
             std::bind(&PlannerNode::local_plan, this));
     }
 
 private:
-    void map_callback(nav_msgs::msg::OccupancyGrid::SharedPtr msg) {
-        if (map_initialized) {
-            return;
-        }
+    Grid map_to_grid(nav_msgs::msg::OccupancyGrid::SharedPtr msg) {
+        Grid grid;
 
-        map_initialized = true;
-
-        RCLCPP_INFO(this->get_logger(), "Received map.");
-        grid = Grid();
-        grid.origin = Pose{msg->info.origin.position.x, msg->info.origin.position.y, 0};
+        grid.data = Eigen::MatrixXf::Zero(msg->info.width, msg->info.height);
+        grid.origin = Pose(msg->info.origin.position.x, msg->info.origin.position.y);
         grid.resolution = msg->info.resolution;
-
-        grid.data = Eigen::MatrixXf(msg->info.width, msg->info.height);
 
         for (int i = 0; i < msg->info.width; i++) {
             for (int j = 0; j < msg->info.height; j++) {
-                // Divide by 100 to get probability of occupancy in the range [0, 1]
-                if (msg->data[j * msg->info.width + i] < 0) {
-                    grid.data(i, j) = -1.0;
-                } else if (msg->data[j * msg->info.width + i] < 50) {
-                    grid.data(i, j) = 0.0;
-                } else {
-                    // grid.data(i, j) = std::min(msg->data[j * msg->info.width + i] / 100.0, 1.0);
-                    grid.data(i, j) = 1.0;
-                }
+                grid.data(i, j) = msg->data[j * msg->info.width + i];
             }
         }
 
-        local_plan_cost = local_plan_cost_generator.generate_cost_map(grid);
-        global_plan_cost = global_plan_cost_generator.generate_cost_map(grid);
+        return grid;
+    }
 
-        RCLCPP_INFO(this->get_logger(), "Generated cost maps.");
-
-        // Convert local plan cost map to an occupancy grid message and publish
-        nav_msgs::msg::OccupancyGrid cost_map_msg;
-        cost_map_msg.header.stamp = this->now();
-        cost_map_msg.header.frame_id = "map";
-        cost_map_msg.info.resolution = grid.resolution;
-        cost_map_msg.info.width = grid.data.cols();
-        cost_map_msg.info.height = grid.data.rows();
-        cost_map_msg.info.origin.position.x = grid.origin.x;
-        cost_map_msg.info.origin.position.y = grid.origin.y;
-        cost_map_msg.info.origin.position.z = 0;
-        cost_map_msg.info.origin.orientation.x = 0;
-        cost_map_msg.info.origin.orientation.y = 0;
-        cost_map_msg.info.origin.orientation.z = 0;
-        cost_map_msg.info.origin.orientation.w = 1;
-
-        cost_map_msg.data.clear();
-
-        int largest_val = 0;
-
-        for (int i = 0; i < grid.data.rows(); i++) {
-            for (int j = 0; j < grid.data.cols(); j++) {
-                if (grid.data(i, j) > largest_val) {
-                    largest_val = grid.data(i, j);
-                }
-            }
-        }
-
-        for (int i = 0; i < grid.data.rows(); i++) {
-            for (int j = 0; j < grid.data.cols(); j++) {
-                cost_map_msg.data.push_back((int)((local_plan_cost->debug_(i, j) / largest_val)
-                                                  * 100));
-            }
-        }
-        nav_msgs::msg::OccupancyGrid mirrored_msg;
-        mirrored_msg.header.stamp = this->now();
-        mirrored_msg.header.frame_id = "map";
-        mirrored_msg.info.resolution = grid.resolution;
-        mirrored_msg.info.width = grid.data.rows();
-        mirrored_msg.info.height = grid.data.cols();
-        mirrored_msg.info.origin.position.z = 0;
-        mirrored_msg.info.origin.orientation.x = 0;
-        mirrored_msg.info.origin.orientation.y = 0;
-        mirrored_msg.info.origin.orientation.z = 0;
-        mirrored_msg.info.origin.orientation.w = 1;
-
-        mirrored_msg.data.clear();
-
-        for (int i = 0; i < grid.data.rows(); i++) {
-            for (int j = 0; j < grid.data.cols(); j++) {
-                mirrored_msg.data.push_back(0);
-            }
-        }
-
-        for (int y = 0; y < cost_map_msg.info.height; ++y) {
-            for (int x = 0; x < cost_map_msg.info.width; ++x) {
-                int new_x = y;
-                int new_y = x;
-                mirrored_msg.data[new_y * cost_map_msg.info.height + new_x] =
-                    cost_map_msg.data[y * cost_map_msg.info.width + x];
-            }
-        }
-
-        // Swap the origin x and y
-        mirrored_msg.info.origin.position.x = cost_map_msg.info.origin.position.x;
-        mirrored_msg.info.origin.position.y = cost_map_msg.info.origin.position.y;
-
-        cost_map_pub_->publish(mirrored_msg);
+    void map_callback(nav_msgs::msg::OccupancyGrid::SharedPtr msg) {
+        map_grid = map_to_grid(msg);
+        this->map = SimpleCostMap(map_grid).toCostmap();
+        this->map_initialized = true;
+    }
+    void costmap_callback(nav_msgs::msg::OccupancyGrid::SharedPtr msg) {
+        this->costmap = SimpleCostMap(map_to_grid(msg)).toCostmap();
+        this->costmap_initialized = true;
     }
 
     void target_callback(geometry_msgs::msg::PoseStamped::SharedPtr msg) {
@@ -185,7 +113,9 @@ private:
 
         odom_pub_->publish(odom);
 
-        if (!map_initialized || !target_initialized) {
+        RCLCPP_INFO(this->get_logger(), "Finished publishing odometry.");
+
+        if (!map_initialized || !costmap_initialized || !target_initialized) {
             RCLCPP_INFO(this->get_logger(), "Waiting for map and target.");
             return;
         }
@@ -202,8 +132,8 @@ private:
         Trajectory initial_guess;
         waypoints.waypoints.push_back(target_state);
 
-        Trajectory path = local_planner->plan_path(grid, current_state, target_state, waypoints,
-            initial_guess, local_plan_cost);
+        Trajectory path = local_planner->plan_path(map_grid, current_state, target_state, waypoints,
+            initial_guess, costmap);
 
         current_state = path.waypoints[1];
 
@@ -227,37 +157,37 @@ private:
         local_path_pub_->publish(nav_path);
     }
 
+    // SETTINGS
     float dt = 0.4;
 
-    Grid grid = Grid();
+    // Map, Costmap
+    Grid map_grid;
+    std::shared_ptr<cost_map::CostMap> map;
+    std::shared_ptr<cost_map::CostMap> costmap;
+    bool map_initialized = false;
+    bool costmap_initialized = false;
+
+    // States, Targets
     State current_state = State(13.0, 298.913, M_PI / 2.0, 0.0, 0.0);
     State target_state = State();
 
-    cost_map::NearestGenerator local_plan_cost_generator = cost_map::NearestGenerator(7, 6);
-    std::shared_ptr<cost_map::CostMap> local_plan_cost;
-
-    cost_map::GlobalCostMapGenerator global_plan_cost_generator =
-        cost_map::GlobalCostMapGenerator(6);
-    std::shared_ptr<cost_map::CostMap> global_plan_cost;
-
-    bool map_initialized = false;
     bool target_initialized = false;
 
+    // Planner
     std::shared_ptr<local_planner::MPC> local_planner;
 
     // ROS
     // Walltimer for `dt` seconds
     rclcpp::TimerBase::SharedPtr timer_;
 
+    // Subscribers
     rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr map_sub_;
-    nav_msgs::msg::OccupancyGrid::SharedPtr map_;
-
-    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
-
+    rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr costmap_sub_;
     rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr target_rviz_sub_;
 
+    // Publishers
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
     rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr local_path_pub_;
-    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr cost_map_pub_;
 
     tf2_ros::TransformBroadcaster tf_broadcast_;
 };
